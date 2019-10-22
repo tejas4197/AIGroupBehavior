7ed9d7111cb6b26403954a9991f1e01c3c0757dd
Tejas Shah <tejas4197@hotmail.com>
2019-10-22 13:37:50 -0400

    scene with health bar

--- Assets/Scripts/AgentHunger.cs
+++ Assets/Scripts/AgentHunger.cs
@@ -1,20 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.AI;
 
 public class AgentHunger : MonoBehaviour
 {
     public float hunger;
+    public float DepletionRate;
+    public Image healthBar;
+    public GameObject foodStore;
 
     private void Start()
     {
         InvokeRepeating("DepleteHealth", 1f, 1f);
+        StartCoroutine(GoToFoodStore());
     }
 
     private void DepleteHealth()
     {
-            Debug.Log(hunger);
-            hunger -= 1;
 
+        Debug.Log(hunger);
+        hunger -= DepletionRate;
+        healthBar.fillAmount = hunger / 100;
+    }
+
+    private IEnumerator GoToFoodStore()
+    { 
+        NavMeshAgent agent = gameObject.GetComponent<NavMeshAgent>();
+        while (true)
+        {
+            if (hunger <= 80)
+            {
+                agent.SetDestination(foodStore.transform.position);
+            }
+        }
     }
 }
+
